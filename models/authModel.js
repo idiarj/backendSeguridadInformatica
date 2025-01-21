6fcb0fdd4cd35d2f144b4dd877818aae53cc4134
idiarj <idiar16@gmail.com>
2025-01-20 23:56:33 -0400

    first commit

---
+++ models/authModel.js
@@ -0,0 +1,14 @@
+
+
+
+
+export class AuthModel {
+
+    static async login({user, email, passowrd}){
+        try {
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+}
