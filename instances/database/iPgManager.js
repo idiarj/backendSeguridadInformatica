918ae651dce50929ab93c404d27bf1d2fa4d7ad9
idiarj <idiar16@gmail.com>
2025-02-05 08:38:35 -0400

    aaaaaaaaaaaa

--- instances/database/iPgManager.js
+++ instances/database/iPgManager.js
@@ -6,12 +6,12 @@ import { FsUtils } from "../../utils/fsUtils.js";
 
 const db_config = await FsUtils.readJsonFile('./config/db-config.json');
 const querys = await FsUtils.readJsonFile('./config/querys.json');
-
+const deployed_db = await FsUtils.readJsonFile('./config/deployed-configDB.json');
 
 
 
 console.log(querys)
-console.log(db_config)
+console.log(deployed_db)
 
-export const appSeguridadInfDB = new PgHandler({config: db_config, querys});
+export const appSeguridadInfDB = new PgHandler({config: deployed_db, querys});
 
