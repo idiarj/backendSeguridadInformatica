3b4c9990e9372d3b6ef1ade6aac0c95a8b503f7e
idiarj <idiar16@gmail.com>
2025-02-10 09:26:53 -0400

    sad

--- routers/applicationRouter.js
+++ routers/applicationRouter.js
@@ -4,8 +4,8 @@ import { authMiddleware } from "../middlewares/auth/authMiddleware.js";
 
 export const applicationRouter = Router();
 
-applicationRouter.get('/admin/:id_user/:user_type', authMiddleware, applicationController.getApplicationsAdmin);
-applicationRouter.get('/user/:id/:id_user', authMiddleware, applicationController.getApplicationsUser);
+applicationRouter.get('/admin/:id_user/:user_type', applicationController.getApplicationsAdmin);
+applicationRouter.get('/user/:id/:id_user',  applicationController.getApplicationsUser);
 applicationRouter.get('/getKey', applicationController.downloadKey);
 applicationRouter.get('/download/:id', applicationController.downloadApplication);
-applicationRouter.put('/update/:id', authMiddleware, applicationController.updateApplicationStatus);
+applicationRouter.put('/update/:id',  applicationController.updateApplicationStatus);
